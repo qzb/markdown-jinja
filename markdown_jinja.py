2a00425e324debed9fdc25941cffbe675ad3afed
Michal Hrusecky <michal.hrusecky@nic.cz>
2019-09-05 13:39:23 +0200

    Allow to specify Filesystem loader in configuration

--- markdown_jinja.py
+++ markdown_jinja.py
@@ -4,13 +4,14 @@
 import json
 from markdown.extensions import Extension
 from markdown.preprocessors import Preprocessor
-from jinja2 import Environment
+from jinja2 import Environment, FileSystemLoader
 
 
 class MarkdownJinja(Extension):
     def __init__(self, configs={}):
         self.config = {
-            'context_file': ['', 'Default location of JSON file containing template context']
+            'context_file': ['', 'Default location of JSON file containing template context'],
+            'fs_loader_dir': ['', 'Default location of templates']
         }
         for key, value in configs.items():
             self.setConfig(key, value)
@@ -24,7 +25,10 @@ class MarkdownJinja(Extension):
 class JinjaPreprocessor(Preprocessor):
     def __init__(self, md, config):
         super(JinjaPreprocessor, self).__init__(md)
-        self.environment = Environment()
+        if config['fs_loader_dir']:
+            self.environment = Environment(loader=FileSystemLoader(config['fs_loader_dir']))
+        else:
+            self.environment = Environment()
         self.context = json.load(open(config['context_file'])) if config['context_file'] else {}
 
     def run(self, lines):
