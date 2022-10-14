db90889e48ef77c708196f6537924b078cc09dfd
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2022-10-14 15:27:25 +0200

    Bump v1.0.0
    * Use Python-Markdown v3.x
    * Fix deprecation warnings with Python-Markdown v3.x
    * Format files with autopep8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='markdown-jinja',
-    version='0.1.0',
+    version='1.0.0',
     py_modules=['markdown_jinja'],
     description='Python Markdown extension which adds Jinja2 support',
     author='Józef Sokołowski',
