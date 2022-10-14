8eb38057b292bc1490191f9d7bda2aa32670142f
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2022-10-14 15:26:41 +0200

    Format files with autopep8

--- markdown_jinja.py
+++ markdown_jinja.py
@@ -25,10 +25,12 @@ class JinjaPreprocessor(Preprocessor):
     def __init__(self, md, config):
         super(JinjaPreprocessor, self).__init__(md)
         if config['fs_loader_dir']:
-            self.environment = Environment(loader=FileSystemLoader(config['fs_loader_dir']))
+            self.environment = Environment(
+                loader=FileSystemLoader(config['fs_loader_dir']))
         else:
             self.environment = Environment()
-        self.context = json.load(open(config['context_file'])) if config['context_file'] else {}
+        self.context = json.load(
+            open(config['context_file'])) if config['context_file'] else {}
 
     def run(self, lines):
         text = '\n'.join(lines)
@@ -37,5 +39,5 @@ class JinjaPreprocessor(Preprocessor):
         return new_text.split('\n')
 
 
-def makeExtension(*args,**kwargs):
+def makeExtension(*args, **kwargs):
     return MarkdownJinja(kwargs)
